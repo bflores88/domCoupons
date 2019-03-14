7378578165b21b53241a88159adbef066f80fce5
kmietotwy <kmietotwy@gmail.com>
2019-03-13 20:48:12 -1000

    doritosss

--- js/app.js
+++ js/app.js
@@ -125,6 +125,11 @@ whatDisc[3].innerHTML = 'Free knee-high tube socks with purchase!';
 //div.price = $7.77
 // button.discount = 33% OFF any 6 pack Budweiser or Bud Light.
 
+whatBrand[4].innerHTML = 'Frito Lay';
+whatItem[4].innerHTML = "Poppin' Jalapeno Doritos";
+whatPrice[4].innerHTML = '$7.77';
+whatDisc[4].innerHTML = '33% OFF any 6 pack Budweiser or Bud Light';
+
 //19. For the Pug, change the contents for each element to the following:
 //div.brand = Any Dog Breeder
 //div.item =  Baby Puggy
